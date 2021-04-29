affb0310bd4ee19bafeb0c55e161fb39081b0438
roy <roy60404@gmail.com>
2021-04-30 04:56:35 +0800

    master

--- B084020053_L03.py
+++ B084020053_L03.py
@@ -1,6 +1,5 @@
 n = int(input("Please input n:"))
 for i in range(1, n+1, 1):
     for j in range(1, n+1, 1):
-        print("%3d" % (i*j), sep="", end="")
+        print("%3d" % (i*j), sep=" ", end=" ")
     print("\n")
-
