770f9297304215002838c30e384d392e4890464b
roy <roy60404@gmail.com>
2021-05-01 16:45:03 +0800

    master

--- hw_3.c
+++ hw_3.c
@@ -8,6 +8,7 @@ int main()
     printf("Input the number: ");
     int data = 0;
     scanf("%d", &data);
+    listSumOfPrimePairs(data);
 }
 
 int IsPrime(int data) //判斷是否為prime如果是,return1,不是return0
@@ -16,6 +17,10 @@ int IsPrime(int data) //判斷是否為prime如果是,return1,不是return0
     if (data == 2 || data == 1)
     {
         return 1;
+    }
+    else if (data == 0)
+    {
+        return 0;
     }
     else
     {
@@ -29,6 +34,20 @@ int IsPrime(int data) //判斷是否為prime如果是,return1,不是return0
         return 1;
     }
 }
-void listSumOfPrimePairs(int data)
+void listSumOfPrimePairs(int data) //一個一個查找質數相加等於data的值並印出來
 {
+    int data1, data2;
+    data1 = data - 1;
+    data2 = data - data1;
+    int i, j;
+    for (i = 0; i < data / 2; i++)
+    {
+        if (IsPrime(data1) && IsPrime(data2))
+        {
+            printf("%d = %d + %d\n", data, data1, data2);
+        }
+        //printf("%d %d %d\n", data, data1, data2);
+        data1 -= 1;
+        data2 += 1;
+    }
 }
