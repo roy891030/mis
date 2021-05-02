93be0e41ee5f7ed3fa21efee7e0485932f9bdf07
roy <roy60404@gmail.com>
2021-05-03 02:28:09 +0800

    master

--- B084020053_Homework/hw_4.c
+++ B084020053_Homework/hw_4.c
@@ -4,35 +4,47 @@ void StrCut(char *);
 void count(char *);
 int main()
 {
+    printf("please enter a string: ");
+    char input[40];
+    scanf("%s", input);
+    //char input[40] = "abbaabab";
+    // char input[40] = "accsdfc";
 
-    // scanf("%s", input);
-    char input[40] = "abababbbabbba";
     printf("%s\n", input);
     StrCut(input);
-    printf("%s\n", input);
 }
 
 void StrCut(char *str) //將input 字串分割成兩兩成對並用空字元隔開
 {
-    int i = 0, count = 0;
-    char new[100];
+    int i = 0;
+    int j = 0;
+    int count = 0;
+    int max = 0;
+    char first = ' ';
+    char second = ' ';
     for (i = 0; i < strlen(str); i++)
     {
-        new[count] = str[i];
-        new[count + 1] = str[i + 1];
-        new[count + 2] = '|';
-        count += 3;
+        for (j = 0; j < strlen(str); j++)
+        {
+            if (str[i] == str[j] && str[j + 1] == str[i + 1])
+            {
+                count++;
+            }
+        }
+
+        if (count >= max)
+        {
+            if (count == max && first < str[i])
+            {
+            }
+            else
+            {
+                max = count;
+                first = str[i];
+                second = str[i + 1];
+            }
+        }
+        count = 0;
     }
-    for (i = 0; i < strlen(new); i++)
-    {
-        str[i] = new[i];
-    }
-}
-void count(char *str)
-{
-    int i, j;
-    for (i = 0; i < strlen(str); i++)
-    {
-        
-    }
+    printf("%c -> %c Ferquency: %d", first, second, max);
 }
